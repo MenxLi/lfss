9bddd866b9bd2095e4a23991db35d0bf3bb8e1e6
limengxun <limengxun45@outlook.com>
2024-10-14 20:22:51 +0800

    explicit readonly connection

--- lfss/src/connection_pool.py
+++ lfss/src/connection_pool.py
@@ -18,13 +18,22 @@ async def execute_sql(conn: aiosqlite.Connection | aiosqlite.Cursor, name: str):
     for s in sql:
         await conn.execute(s)
 
-async def get_connection() -> aiosqlite.Connection:
+async def get_connection(read_only: bool = False) -> aiosqlite.Connection:
     if not os.environ.get('SQLITE_TEMPDIR'):
         os.environ['SQLITE_TEMPDIR'] = str(DATA_HOME)
-    # large blobs are stored in a separate database, should be more efficient
-    conn = await aiosqlite.connect(DATA_HOME / 'index.db', timeout = 60)
+
+    def get_db_uri(path: Path, read_only: bool = False):
+        return f"file:{path}?mode={ 'ro' if read_only else 'rwc' }"
+
+    conn = await aiosqlite.connect(
+        get_db_uri(DATA_HOME / 'index.db', read_only=read_only), 
+        timeout = 60, uri = True
+        )
     async with conn.cursor() as c:
-        await c.execute(f"ATTACH DATABASE ? AS blobs", (str(DATA_HOME/'blobs.db'), ))
+        await c.execute(
+            f"ATTACH DATABASE ? AS blobs", 
+            (get_db_uri(DATA_HOME/'blobs.db', read_only=read_only), )
+            )
     await execute_sql(conn, 'pragma.sql')
     return conn
 
@@ -35,47 +44,49 @@ class SqlConnection:
     is_available: bool = True
 
 class SqlConnectionPool:
-    _sem: Semaphore
+    _r_sem: Semaphore
     _w_sem: Semaphore
     def __init__(self):
-        self._connections: list[SqlConnection] = []
-        self._w_connection: None | SqlConnection = None
+        self._readers: list[SqlConnection] = []
+        self._writer: None | SqlConnection = None
         self._lock = Lock()
     
     async def init(self, n_read: int):
         await self.close()
-        self._connections = []
-        for _ in range(n_read):
-            conn = await get_connection()
-            self._connections.append(SqlConnection(conn))
-        self._w_connection = SqlConnection(await get_connection())
-        self._sem = Semaphore(n_read)
+        self._readers = []
+
+        self._writer = SqlConnection(await get_connection(read_only=False))
         self._w_sem = Semaphore(1)
+
+        for _ in range(n_read):
+            conn = await get_connection(read_only=True)
+            self._readers.append(SqlConnection(conn))
+        self._r_sem = Semaphore(n_read)
     
     @property
     def n_read(self):
-        return len(self._connections)
+        return len(self._readers)
     @property
-    def sem(self):
-        return self._sem
+    def r_sem(self):
+        return self._r_sem
     @property
     def w_sem(self):
         return self._w_sem
     
     async def get(self, w: bool = False) -> SqlConnection:
-        if len(self._connections) == 0:
+        if len(self._readers) == 0:
             raise Exception("No available connections, please init the pool first")
         
         async with self._lock:
             if w:
-                assert self._w_connection
-                if self._w_connection.is_available:
-                    self._w_connection.is_available = False
-                    return self._w_connection
+                assert self._writer
+                if self._writer.is_available:
+                    self._writer.is_available = False
+                    return self._writer
                 raise Exception("Write connection is not available")
 
             else:
-                for c in self._connections:
+                for c in self._readers:
                     if c.is_available:
                         c.is_available = False
                         return c
@@ -83,19 +94,19 @@ class SqlConnectionPool:
     
     async def release(self, conn: SqlConnection):
         async with self._lock:
-            if conn == self._w_connection:
+            if conn == self._writer:
                 conn.is_available = True
                 return
 
-            if not conn in self._connections:
+            if not conn in self._readers:
                 raise Exception("Connection not in pool")
             conn.is_available = True
     
     async def close(self):
-        for c in self._connections:
+        for c in self._readers:
             await c.conn.close()
-        if self._w_connection:
-            await self._w_connection.conn.close()
+        if self._writer:
+            await self._writer.conn.close()
 
 # these two functions shold be called before and after the event loop
 g_pool = SqlConnectionPool()
@@ -125,7 +136,7 @@ def global_entrance(n_read: int = 1):
 @asynccontextmanager
 async def unique_cursor(is_write: bool = False):
     if not is_write:
-        async with g_pool.sem:
+        async with g_pool.r_sem:
             connection_obj = await g_pool.get()
             try:
                 yield await connection_obj.conn.cursor()

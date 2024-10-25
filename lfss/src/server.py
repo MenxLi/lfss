dac746343e4fe2d189ff2cb0494452c595c03693
limengxun <limengxun45@outlook.com>
2024-10-25 10:32:27 +0800

    starting sign

--- lfss/src/server.py
+++ lfss/src/server.py
@@ -32,6 +32,7 @@ async def lifespan(app: FastAPI):
         await global_connection_init(n_read = 2)
         await asyncio.gather(db.init(), req_conn.init())
         await req_conn.shrink()
+        logger.info("Server started.")
         yield
         await req_conn.commit()
     finally:

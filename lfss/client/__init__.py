45a528a0f6d8eff65b8e57d3c963becf694584fb
limengxun <limengxun45@outlook.com>
2024-11-12 18:22:02 +0800

    include error msg; fix empty file download

--- lfss/client/__init__.py
+++ lfss/client/__init__.py
@@ -12,8 +12,9 @@ def upload_file(
     interval: float = 0, 
     verbose: bool = False, 
     **put_kwargs
-    ):
+    ) -> tuple[bool, str]:
     this_try = 0
+    error_msg = ""
     while this_try <= n_retries:
         try:
             with open(file_path, 'rb') as f:
@@ -25,6 +26,7 @@ def upload_file(
                 raise e
             if verbose:
                 print(f"Error uploading {file_path}: {e}, retrying...")
+                error_msg = str(e)
             this_try += 1
         finally:
             time.sleep(interval)
@@ -32,8 +34,8 @@ def upload_file(
     if this_try > n_retries:
         if verbose:
             print(f"Failed to upload {file_path} after {n_retries} retries.")
-        return False
-    return True
+        return False, error_msg
+    return True, error_msg
 
 def upload_directory(
     connector: Connector, 
@@ -44,7 +46,7 @@ def upload_directory(
     interval: float = 0,
     verbose: bool = False,
     **put_kwargs
-    ) -> list[str]:
+    ) -> list[tuple[str, str]]:
     assert path.endswith('/'), "Path must end with a slash."
     if path.startswith('/'):
         path = path[1:]
@@ -53,7 +55,7 @@ def upload_directory(
     _counter = 0
     _counter_lock = Lock()
 
-    faild_files = []
+    faild_items = []
     def put_file(c: Connector, file_path):
         with _counter_lock:
             nonlocal _counter
@@ -63,11 +65,11 @@ def upload_directory(
             if verbose:
                 print(f"[{this_count}] Uploading {file_path} to {dst_path}")
 
-        if not upload_file(
+        if not (res:=upload_file(
             c, file_path, dst_path, 
             n_retries=n_retries, interval=interval, verbose=verbose, **put_kwargs
-            ):
-            faild_files.append(file_path)
+            ))[0]:
+            faild_items.append((file_path, res[1]))
 
     with connector.session(n_concurrent) as c:
         with BoundedThreadPoolExecutor(n_concurrent) as executor:
@@ -75,7 +77,7 @@ def upload_directory(
                 for file in files:
                     executor.submit(put_file, c, os.path.join(root, file))
 
-    return faild_files
+    return faild_items
 
 def download_file(
     connector: Connector, 
@@ -85,17 +87,19 @@ def download_file(
     interval: float = 0, 
     verbose: bool = False, 
     overwrite: bool = False
-    ):
+    ) -> tuple[bool, str]:
     this_try = 0
+    error_msg = ""
     while this_try <= n_retries:
         if not overwrite and os.path.exists(file_path):
             if verbose:
                 print(f"File {file_path} already exists, skipping download.")
-            return True
+            return True, error_msg
         try:
             blob = connector.get(src_url)
-            if not blob:
-                return False
+            if blob is None:
+                error_msg = "File not found."
+                return False, error_msg
             pathlib.Path(file_path).parent.mkdir(parents=True, exist_ok=True)
             with open(file_path, 'wb') as f:
                 f.write(blob)
@@ -105,6 +109,7 @@ def download_file(
                 raise e
             if verbose:
                 print(f"Error downloading {src_url}: {e}, retrying...")
+                error_msg = str(e)
             this_try += 1
         finally:
             time.sleep(interval)
@@ -112,8 +117,8 @@ def download_file(
     if this_try > n_retries:
         if verbose:
             print(f"Failed to download {src_url} after {n_retries} retries.")
-        return False
-    return True
+        return False, error_msg
+    return True, error_msg
 
 def download_directory(
     connector: Connector,
@@ -124,7 +129,7 @@ def download_directory(
     interval: float = 0,
     verbose: bool = False,
     overwrite: bool = False
-    ) -> list[str]:
+    ) -> list[tuple[str, str]]:
 
     directory = str(directory)
 
@@ -135,9 +140,9 @@ def download_directory(
     
     _counter = 0
     _counter_lock = Lock()
-    failed_files = []
+    failed_items: list[tuple[str, str]] = []
     def get_file(c, src_url):
-        nonlocal _counter, failed_files
+        nonlocal _counter, failed_items
         with _counter_lock:
             _counter += 1
             this_count = _counter
@@ -145,14 +150,14 @@ def download_directory(
             if verbose:
                 print(f"[{this_count}] Downloading {src_url} to {dst_path}")
 
-        if not download_file(
+        if not (res:=download_file(
             c, src_url, dst_path, 
             n_retries=n_retries, interval=interval, verbose=verbose, overwrite=overwrite
-            ):
-            failed_files.append(src_url)
+            ))[0]:
+            failed_items.append((src_url, res[1]))
         
     with connector.session(n_concurrent) as c:
         with BoundedThreadPoolExecutor(n_concurrent) as executor:
             for file in c.list_path(src_path, flat=True).files:
                 executor.submit(get_file, c, file.url)
-    return failed_files
+    return failed_items

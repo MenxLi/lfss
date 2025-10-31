8b989c34f21bfeab07e37907410908736247c1db
limengxun <limengxun45@outlook.com>
2025-10-31 16:28:31 +0800

    use wal mode

--- lfss/sql/pragma.sql
+++ lfss/sql/pragma.sql
@@ -1,8 +1,8 @@
-PRAGMA journal_mode=DELETE;
+PRAGMA journal_mode=WAL;
 PRAGMA synchronous=NORMAL;
 PRAGMA case_sensitive_like=ON;
 PRAGMA page_size=4096;
 
-PRAGMA blobs.journal_mode=DELETE;
+PRAGMA blobs.journal_mode=WAL;
 PRAGMA blobs.synchronous=NORMAL;
 PRAGMA blobs.page_size=16384;

5dd1300b8cae4c4e6496ce6ba47e15d98dda25c1
limengxun <limengxun45@outlook.com>
2025-11-04 18:23:44 +0800

    add built doc

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "LFSS",
   description: "LFSS Documentation",
+  base: '/.docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
--- lfss/svc/app.py
+++ lfss/svc/app.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
 from .app_base import ENABLE_WEBDAV
 from .app_native import *
 from .app_native_user import *
 
+__src_root = Path(__file__).parent.parent
+__doc_path = __src_root.parent / "docs" / ".vitepress" / "dist"
+assert __doc_path.parent.exists()
+if not __doc_path.exists():
+    __doc_path.mkdir()
+    with open(__doc_path / "index.html", "w") as f:
+        f.write("<h1>Documentation not built yet.</h1><p>Please build the documentation first.</p>")
+
 # order matters
+app.mount("/.docs", StaticFiles(directory=__doc_path), name="docs")
 app.include_router(router_user, prefix="/_api/user")
 app.include_router(router_user, prefix="/.api/user")
 app.include_router(router_api, prefix="/_api")
